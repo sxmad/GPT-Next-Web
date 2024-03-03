8e0fb1402bb957305f795b1ffd043a52c9b9cc31
asdfq <sxmad@163.com>
2024-03-03 10:59:49 +0800

    update

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          aapp
+          www1.app
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          aapp = a app
+          your own ai
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
